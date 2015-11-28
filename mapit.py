d2c1e8f2713708baf701b59ae1eeb69c30e9ea8f
cswiger <cswiger@gmail.com>
2015-11-28 17:50:15 -0500

    Create mapit.py
    A utility to make CarPal log files usable with MapBox to plotting position fixes and one PID such as speed.

---
+++ mapit.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# easy python script to convert a CarPal log file from json to csv to csv for uploading to MapBox
+# looks for 'latlng' position and currently 'speed' in the json - MapBox allows 1 label per marker
+
+import sys
+import json
+
+if len(sys.argv) != 3:
+  print("Usage: ./mapit.py in.json out.csv")
+  sys.exit(1)
+
+fh = open(sys.argv[1],'r')
+
+line = fh.readline()
+jtrip = json.loads(line)
+line = fh.readline()
+while line != '':
+  jtrip += json.loads(line)
+  line = fh.readline()
+
+fh.close()
+fh = open(sys.argv[2],'w')
+fh.write("latitude,longitude,speed\n")
+for i in range(len(jtrip)):
+   fh.write(str(jtrip[i]["latlng"][0]) + "," + str(jtrip[i]["latlng"][1]) + "," + str(jtrip[i]["speed"]) + "\n")
+
+fh.close()
+
